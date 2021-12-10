c5dee7a742741b0596d66d4b72528770852f2b3b
Anton Volkov <thevadxx@gmail.com>
2021-12-11 00:19:59 +0300

    Update use alias in script

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ TMUX_C=.tmux.conf
 mkdir -p $NVIM_DIR
 
 chsh -s /usr/bin/fish
+source $FISH_DIR/config.fish
+tmux source-file ~/$TMUX_C
 curl -o ~/$TMUX_C https://raw.githubusercontent.com/vadxx/config/main/$TMUX_C
 curl -o $FISH_DIR/config.fish https://raw.githubusercontent.com/vadxx/config/main/config.fish 
 curl -o $NVIM_DIR/init.vim https://raw.githubusercontent.com/vadxx/config/main/init.vim
@@ -12,5 +14,4 @@ curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --c
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vi +PlugInstall +qall!
 
-source $FISH_DIR/config.fish 
-tmux source-file ~/$TMUX_C
+
