d061b59a7b201a506947780221e5ca0f7065305d
Anton Volkov <thevadxx@gmail.com>
2021-12-11 00:21:49 +0300

    Upd sequende of script

--- setup.sh
+++ setup.sh
@@ -5,13 +5,16 @@ TMUX_C=.tmux.conf
 mkdir -p $NVIM_DIR
 
 chsh -s /usr/bin/fish
-source $FISH_DIR/config.fish
-tmux source-file ~/$TMUX_C
+mkdir -p $NVIM_DIR $FISH_DIR
+
 curl -o ~/$TMUX_C https://raw.githubusercontent.com/vadxx/config/main/$TMUX_C
 curl -o $FISH_DIR/config.fish https://raw.githubusercontent.com/vadxx/config/main/config.fish 
 curl -o $NVIM_DIR/init.vim https://raw.githubusercontent.com/vadxx/config/main/init.vim
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+source $FISH_DIR/config.fish
+tmux source-file ~/$TMUX_C
 vi +PlugInstall +qall!
 
 
