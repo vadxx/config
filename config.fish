8e1f437187d91fd2e4c67e7d788124879be7dae7
Anton Volkov <thevadxx@gmail.com>
2022-04-05 14:33:22 +0300

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,12 +1,13 @@
-alias exa="exa --icons --header --git"
-alias ls="exa -G"
-alias la="exa -a"
-alias ll="exa -l"
-alias lt="exa -T"
-alias g="git"
-alias vi="nvim"
-alias vim="nvim"
+alias exa "exa --icons --header --git"
+alias ls "exa -G"
+alias la "exa -a"
+alias ll "exa -l"
+alias l "exa -TL2"
+alias lt "exa -TL"
+alias g git
+alias vi nvim
+alias vim nvim
 
-export EDITOR="nvim"
-export VISUAL="nvim"
+export EDITOR=nvim
+export VISUAL=nvim
 export TERM="xterm-256color"
